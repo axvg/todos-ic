0e2679904844febe844ca4fd88a28c08897e17cf
axvg <91773368+axvg@users.noreply.github.com>
2023-07-01 17:42:10 -0500

    home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+export interface Item {
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +12,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) {}
+  public items!: Item[];
 
-  constructor() {}
+  ionViewDidEnter() {
+    this.items = [
+      {
+        title: 'Item 1',
+        description: 'This is an item description.',
+      },
+      {
+        title: 'Item 2',
+        description: 'This is an item description.',
+      },
+      {
+        title: 'Item 3',
+        description: 'This is an item description.',
+      },
+    ];
+  }
 
+  viewItem(item: any) {}
+  addItem() {}
 }
